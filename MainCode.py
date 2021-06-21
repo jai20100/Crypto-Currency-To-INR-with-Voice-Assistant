cd68864b56e3c1b6dd96d3f652f8836e68599834
jai20100 <85556510+jai20100@users.noreply.github.com>
2021-06-21 16:11:26 +0530

    Half Comments

--- MainCode.py
+++ MainCode.py
@@ -9,29 +9,35 @@ from num2words import num2words
 from gtts import gTTS
 import os
 
-# bfvhbfvh
+
 #class with name CurrencyConverter
 class CurrencyConverter(tk.Frame):
     # Here we define a function with name __init__ and parameters as shown
+    
     def __init__(self,parent,*args,**kwargs):
         tk.Frame.__init__(self,parent,*args,**kwargs)
-       # Here we declared all variable names using StringVar()
+        
+ #We plot grid here and set font of parent
+  
         self.grid(padx=(20,30), pady= (30,20))
         self.app_font = ("Helvetica", 20, "bold")
         self.parent = parent
         self.parent.title("CryptoCurrencyConverter(WithVoiceAssistant)")
         self.parent.columnconfigure(0,weight=2)
+        
+# Here we declared all required variable names using StringVar() function 
+  
         self.base_currency = tk.StringVar()
-        self.base_currency.set("Base Currency")
+        self.base_currency.set("Base Currency")   # stores Base currency, which we want to convert
         self.target_currency = tk.StringVar()
-        self.target_currency.set("Target Currency")
+        self.target_currency.set("Target Currency") # Stores Target currency, To which we want to covert
         self.base_text = tk.StringVar()
-        self.base_text.set("From:")
+        self.base_text.set("From:")               
         self.target_text = tk.StringVar()
         self.target_text.set("To:")
         self.amount = tk.StringVar()
         self.amount.set("0")
-        self.j1 = tk.StringVar()
+        self.j1 = tk.StringVar()  
         self.j1.set("")
         self.Result_value1 = tk.StringVar()
         self.Result_value1.set("0")
@@ -41,26 +47,35 @@ class CurrencyConverter(tk.Frame):
         self.Choice_from.set("BTC")
         self.Choice_To = tk.StringVar()
         self.Choice_To.set("INR")
+ 
+#We listed only top currencies here and stored in chocies
 
         Choice1 = {"INR", "USD", "EUR", "GBP","CAD", "DKK"}
         Choice2 = {"BTC", "ETH","BNB", "USDT","USDC", "BUSD"}
-        
+    
+# Here we Define Lables like "FROM" "TO"  and PopUpMenu  
         Label_Source = tk.Label(self, textvariable=self.base_text, fg="black",bg="grey", font= "comicsansms 14 bold" )
         Label_Source.grid(row=0, column=0)
-        popupMenuF = OptionMenu(self, self.Choice_from, *Choice2)
+        popupMenuF = OptionMenu(self, self.Choice_from, *Choice2) #Popup menu of FROM Currencies
         popupMenuF.grid(row=0,column=1)
 
         Label_Target = tk.Label(self, textvariable=self.target_text, fg="black",bg="grey",font= "comicsansms 14 bold")
         Label_Target.grid(row=0, column=5)
-        popupMenuTO = OptionMenu(self, self.Choice_To, *Choice1)
+        popupMenuTO = OptionMenu(self, self.Choice_To, *Choice1) #Popup menu of TO Currencies
         popupMenuTO.grid(row=0,column=7)
+
+#Defining Final Result Displaying Label
 
         Label_Result = tk.Label(self, textvariable= self.Result_value2, fg="black", font=self.app_font)
         Label_Result.grid(row=1, column=8)
+    
+#Here We take input from user as an interger 
 
         self.input__ = tk.Label(self, text ="Enter", font=self.app_font, width=8)
-        self.input__= Entry(jairoot)
+        self.input__= Entry(jairoot) #stores the values in input__
         self.input__.place(x=55, y=65)
+
+#Here we define Coverting Button and give Commands To them
 
         Converting_Button = tk.Button(self, text="Convert", command=self.Convert, bg="green", fg="black",width=6, height=1,borderwidth=4, relief=GROOVE)
         Converting_Button.grid(row=9, column=1)
@@ -69,16 +84,18 @@ class CurrencyConverter(tk.Frame):
         Audio_Button = tk.Button(self,text="Hear It", command=self.Hear, bg="blue", fg="black", width=6, height=1,borderwidth=4, relief=GROOVE)
         Audio_Button.grid(row=9, column=4)
 
+#Here We give Functionality to Covert button
     def Convert(self):
         if(self.input__.get()==""):
-            Result = "ERROR: Enter any Value to Convert"
+            Result = "ERROR: Enter any Value to Convert" #If user gives nothing in input and hit convert button There shows an error
             self.Result_value2.set(Result)
         else:
-            from__ = self.Choice_from.get()
-            to__ = self.Choice_To.get()
+            from__ = self.Choice_from.get() # gets choosen FROM currency
+            to__ = self.Choice_To.get() # gets choosen TO currency
             response = requests.get(f"https://api.nomics.com/v1/exchange-rates?key=d2ed4bee6312d6dc37e5f52ee2fbdc254d18d556")
-            Data = response.json()
-            l1= list(filter(lambda person: person['currency'] == from__, Data))
+#Pulls Data from API and stores in Data
+            Data = response.json() # converts HTML response to json Format
+            l1= list(filter(lambda person: person['currency'] == from__, Data)) 
             l2 = list(filter(lambda person: person['currency'] == to__, Data)) 
             a_key = "rate"
             v1 = [dict[a_key] for dict in l1]
@@ -95,7 +112,7 @@ class CurrencyConverter(tk.Frame):
 
     def Hear(self):
         if(self.input__.get()==""):
-            Result = "ERROR: Enter any Value to Hear"
+            Result = "ERROR: Enter any Value to Hear" #If user gives nothing in input and hit convert button There shows an error
             self.Result_value2.set(Result)
         else:
             r1 = self.Result_value1.get()
